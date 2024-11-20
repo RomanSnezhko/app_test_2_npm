6b812b9453d28069198c5371235d7649ffd55753
RomanSnezhko <roman.snezhko@bk.ru>
2024-11-20 21:21:34 +0300

    add Kata2

--- CodeWars/KataSQL.tsx
+++ CodeWars/KataSQL.tsx
@@ -14,3 +14,18 @@ NOTE: The United States is written as 'USA' in the table.
 NOTE: Your solution should use pure SQL. Ruby is used within the test cases just to validate your answer.
 */
 select * from travelers where not country in ('Mexico', 'Canada', 'USA') ;
+
+/*
+8 est-Selling Books (SQL for Beginners #5)
+\ou work at a book store. It's the end of the month, and you need to find out the 5 bestselling books at your store. Use a select statement to list names, authors, and number of copies sold of the 5 books which were sold most.
+
+books table schema
+
+name
+author
+copies_sold
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases just to validate your answer.
+
+*/
+
+select * from books order by copies_sold desc limit 5;
