d66d8c12278f0df8b98b6899066b1fee5811bff2
RomanSnezhko <roman.snezhko@bk.ru>
2024-11-22 00:22:57 +0300

    add Kata11

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -149,3 +149,23 @@ Solution:
     */
 
     const friend = friends => friends. filter(element => element. length == 4)
+
+    /*
+    8 kyu Remove duplicates from list
+
+    Description:
+
+    MDefine a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+    Solution:
+        */
+    function distinct(a) {
+        return [...new Set (a)];
