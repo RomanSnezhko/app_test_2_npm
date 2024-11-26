ca6a6f032148a9f11577df90855ebd7f641c7280
RomanSnezhko <roman.snezhko@bk.ru>
2024-11-26 19:02:09 +0300

    add Kata16

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -284,15 +284,33 @@ Solution:
 */
         const removeUrlAnchor = url => url.replace(/#.+/i, "");
 /*
-7 kyu
+7 kyu Сountdown to Christm
 
 Description:
 
+Polly is 8 years old. She is eagerly awaiting Christmas as she has a bone to pick with Santa Claus. Last year she asked for a horse, and he brought her a dolls house. Understandably she is livid.
 
+The days seem to drag and drag so Polly asks her friend to help her keep count of how long it is until Christmas, in days. She will start counting from the first of December.
+
+Your function should take 1 argument (a Date object) which will be the day of the year it is currently. The function should then work out how many days it is until Christmas.
+
+Watch out for leap years!
 
 Solution:
 */
+        function daysUntilChristmas(days) {
+            let year  = days.getFullYear(),
+                given = days.getTime() / 1000,
+                final = new Date(year, 11, 25, 0, 0, 0) / 1000,
+                loopGiven = null;
 
+            if(given > final) {
+                loopGiven = new Date(year + 1, 11, 25, 0, 0, 0) / 1000;
+                return Math.abs((final - loopGiven) / 86400) - Math.abs((final - given) / 86400);
+            }
+
+            return Math.abs((final - given) / 86400);
+        }
 /*
 7 kyu
 
