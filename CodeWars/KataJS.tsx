8081d9a509a1c34ae2619b81560a2bf9f1f031bf
RomanSnezhko <roman.snezhko@bk.ru>
2024-11-27 21:09:57 +0300

    add Kata 18

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -63,7 +63,7 @@ Example(Input1, Input2 --> Output)
 Solution:
 */
 function divisibleBy(numbers, divisor){
-    return numbers. filter ((number) => number % divisor === 0)
+    return numbers. filter (number) => number % divisor === 0)
 }
 
 
@@ -349,15 +349,26 @@ Solution:
             return array.map(item => Math.round( mult / (1 / item )));
         }
 /*
-7 kyu
+6 kyu Create Phone Number
 
 Description:
 
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 
+Example
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!
 
 Solution:
 */
-
+        function createPhoneNumber(numbers) {
+            let result = "(";
+            for(let i = 0; i < numbers.length; i++) result += (i === 2) ? `${numbers[i]}) ` : (i === 5) ? `${numbers[i]}-` : numbers[i];
+            return result;
+        }
 /*
 7 kyu
 
