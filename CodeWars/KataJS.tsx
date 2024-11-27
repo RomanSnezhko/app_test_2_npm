e59209ca5c9183016c0603a4bbd9921c78ee476e
RomanSnezhko <roman.snezhko@bk.ru>
2024-11-27 21:11:17 +0300

    add Kata 19

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -370,15 +370,34 @@ Solution:
             return result;
         }
 /*
-7 kyu
+5 kyu Maximum subarray sum
 
 Description:
 
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
 
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+// should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 
 Solution:
 */
+        const maxSequence = array => {
+            if(array.length === 0 || array.every(item => item <= 0)) return 0;
 
+            let maxResult = 0,
+                subResult = 0;
+
+            for(let item of array) {
+                subResult += item;
+                maxResult = Math.max(maxResult, subResult);
+                if(subResult < 0) subResult = 0;
+            }
+
+            return maxResult;
+        };
 /*
 7 kyu
 
