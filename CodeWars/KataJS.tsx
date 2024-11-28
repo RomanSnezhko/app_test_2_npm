3e402a695ce4cf961b0ccff63143550e9348332e
RomanSnezhko <roman.snezhko@bk.ru>
2024-11-28 22:15:57 +0300

    add Kata 20

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -80,6 +80,7 @@ Example(Input --> Output)
 
 Solution:
 */
+// @ts-ignore
 function nameShuffler (str){
     let [first, last] = str. split(' ')
     return last + ' ' + first
@@ -98,7 +99,8 @@ All values will be positive integers or floats, or zero.
 
 Solution:
 */
-function plural(n) {
+// @ts-ignore
+function plural( n) {
     return n !== 1;
 }
 
@@ -399,7 +401,32 @@ Solution:
             return maxResult;
         };
 /*
-7 kyu
+6 kyu Convert string to camel case
+
+Description:
+
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+Examples
+
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+Solution:
+*/
+
+        function toCamelCase(str){
+            var regExp=/[-_]\w/ig;
+            return str.replace(regExp,function(match){
+                return match.charAt(1).toUpperCase();
+            });
+        }
+
+/*
+6 kyu
 
 Description:
 
@@ -409,7 +436,7 @@ Solution:
 */
 
 /*
-7 kyu
+6 kyu
 
 Description:
 
@@ -419,7 +446,87 @@ Solution:
 */
 
 /*
-7 kyu
+6 kyu
+
+Description:
+
+
+
+Solution:
+*/
+
+/*
+6 kyu
+
+Description:
+
+
+
+Solution:
+*/
+
+/*
+6 kyu
+
+Description:
+
+
+
+Solution:
+*/
+
+/*
+6 kyu
+
+Description:
+
+
+
+Solution:
+*/
+
+/*
+6 kyu
+
+Description:
+
+
+
+Solution:
+*/
+
+/*
+6 kyu
+
+Description:
+
+
+
+Solution:
+*/
+
+/*
+6 kyu
+
+Description:
+
+
+
+Solution:
+*/
+
+/*
+6 kyu
+
+Description:
+
+
+
+Solution:
+*/
+
+/*
+6 kyu
 
 Description:
 
