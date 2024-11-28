6b7662b453257e6b642863df15aa75ee559045ee
RomanSnezhko <roman.snezhko@bk.ru>
2024-11-28 22:21:46 +0300

    add Kata SQL 6

--- CodeWars/KataSQL.tsx
+++ CodeWars/KataSQL.tsx
@@ -48,15 +48,31 @@ Solution:
 */
 select (number1 % number2) as mod from decimals;xx
 /*
-7 kyu
+6 kyu Youngest Team Member
 
 Description:
 
+You are working with a database that stores information about employees in a tech firm. The database includes a table named employees with the following columns:
 
+employee_id: A unique integer identifier for each employee.
+full_name: A string representing the employee's full name.
+team: A string that specifies which team the employee is part of. The team can be one of the following four: "backend", "frontend", "devops", or "design".
+birth_date: A date that represents the employee's birthdate.
+The company is planning an event where the youngest employee from each team will be given a chance to share their vision of future technology trends.
+
+Your task is to write an SQL query that retrieves the complete record for the youngest member of each team. You should consider the person with the latest birthdate as the youngest. Let's assume for this task that the are no youngest employees who share the same birthdate.
+
+The classical solution of using aggregate function and group by is forbidden. Can you come up with something more witty?
+
+The result should be ordered by team in asc alphabetical order.
+
+Good luck!
 
 Solution:
 */
-
+select distinct on (team) employee_id, full_name, team, birth_date
+from employees
+order by team, birth_date desc
 /*
 7 kyu
 
