47c511e7e399decec3cd343a90b89ee1b402655e
RomanSnezhko <roman.snezhko@bk.ru>
2024-12-01 20:32:46 +0300

    add Battleship v 1.1

--- Battleship/main.js
+++ Battleship/main.js
@@ -1,20 +1,31 @@
 
-    const p1 = 2; // позиционирование корабля
-    const p2 = 3;
-    const p3 = 4;
+    const posicion = Math.floor(Math.random() * 5);                    // рандомная позиция
 
-    kill = 0;     // счетчик попаданий
+    const p2 = posicion + 1;
+    const p3 = p2 + 1;
 
-    let playerShot    //  выстрел игрока
+    let numberShots = 0;
+    let kill = 0;
+    let playerShot;
 
-    while (kill < 3) {
-
-        playerShot = +prompt("Your shot", '')
-        if (playerShot === p1 || playerShot === p2 || playerShot === p3) {
-            alert('OOO, my blood')
-            kill = kill + 1}
-        else {
-            alert("Your are looser!")
+    while (kill < 3)
+    {
+        playerShot = +prompt("Your shot 1-7", '');
+        if (playerShot <= 0 || playerShot > 7)
+        {
+            alert("Chose shot 1-7");
+        } else
+            {
+            if (playerShot === posicion || playerShot === p2 || playerShot === p3)
+                {
+                    alert('OOO, my blood')
+                    kill = kill + 1
+                }   else {
+                alert("Your are looser!")
+            }
+                numberShots = numberShots + 1;
         }
+
     }
-    alert('Your are Kill me, little bich!')
+    alert('Your are Kill me, little bitch!')
+    alert ('Your needed ' + numberShots + ' shots' )
