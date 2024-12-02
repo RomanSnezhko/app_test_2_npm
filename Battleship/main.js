882934e5ff979619f414e2f758830d17ce3a285b
RomanSnezhko <roman.snezhko@bk.ru>
2024-12-02 19:51:48 +0300

    add Battleship v 1.2

--- Battleship/main.js
+++ Battleship/main.js
@@ -1,31 +1,31 @@
 
-    const posicion = Math.floor(Math.random() * 5);                    // рандомная позиция
+    // const posicion = Math.floor(Math.random() * 5);                    // рандомная позиция
+    //
+    // const p2 = posicion + 1;
+    // const p3 = p2 + 1;
+    //
+    // let numberShots = 0;
+    // let kill = 0;
+    // let playerShot;
 
-    const p2 = posicion + 1;
-    const p3 = p2 + 1;
-
-    let numberShots = 0;
-    let kill = 0;
-    let playerShot;
-
-    while (kill < 3)
-    {
-        playerShot = +prompt("Your shot 1-7", '');
-        if (playerShot <= 0 || playerShot > 7)
-        {
-            alert("Chose shot 1-7");
-        } else
-            {
-            if (playerShot === posicion || playerShot === p2 || playerShot === p3)
-                {
-                    alert('OOO, my blood')
-                    kill = kill + 1
-                }   else {
-                alert("Your are looser!")
-            }
-                numberShots = numberShots + 1;
-        }
-
-    }
-    alert('Your are Kill me, little bitch!')
-    alert ('Your needed ' + numberShots + ' shots' )
+    // while (kill < 3)
+    // {
+    //     playerShot = +prompt("Your shot 1-7", '');
+    //     if (playerShot <= 0 || playerShot > 7)
+    //     {
+    //         alert("Chose shot 1-7");
+    //     } else
+    //         {
+    //         if (playerShot === posicion || playerShot === p2 || playerShot === p3)
+    //             {
+    //                 alert('OOO, my blood')
+    //                 kill = kill + 1
+    //             }   else {
+    //             alert("Your are looser!")
+    //         }
+    //             numberShots = numberShots + 1;
+    //     }
+    //
+    // }
+    // alert('Your are Kill me, little bitch!')
+    // alert ('Your needed ' + numberShots + ' shots' )
