ed10c2e9d215b03740eea1100352c9c1afdc9cd8
RomanSnezhko <roman.snezhko@bk.ru>
2025-01-24 17:48:07 +0300

    add array shopKorona

--- JS start/main.js
+++ JS start/main.js
@@ -37,120 +37,223 @@
 // Test 2
 
 
-let myRoom = [
-    {
-        material: "fabric",
-        color: "gray",
-        size: "lage",
-        weight:
-            {
-                scale: "kg",
-                volume: 50
-            },
-        position: "right wall",
-        coast:
-            {
-                currency: "BYN",
-                volume: 1500
-            }
-    }, {
-        material: "tree",
-        color: "yellow",
-        size: "medium",
-        weight:
-            {
-                scale: "kg",
-                volume: 10
-            },
-        position: "window",
-        coast:
-            {
-                currency: "BYN",
-                volume: 250
-            },
-    }, {
-        material: "skin",
-        color: "brown",
-        size: "semi-medium",
-        weight: {
-            scale: "kg",
-            volume: 15
-        },
-        position: "center",
-        coast:
-            {
-                currency: "BYN",
-                volume: 850
-            }
-    }, {
-        material: "metal",
-        color: "black",
-        size: "small",
-        weight: {
-            scale: "kg",
-            volume: 0.3
-        },
-        position: "on the table",
-        coast: {
-            currency: "BYN",
-            volume: 25
-        }
-    }, {
-        material: "skin",
-        color: "purple",
-        size: "semi medium",
-        weight: {
-            scale: "kg",
-            volume: 5
-        },
-        position: "left angle",
-        coast: {
-            currency: "BYN",
-            volume: 380
-        }
-    }, {
-        material: "tree",
-        color: "light brown",
-        size: "medium",
-        weight: {
-            scale: "kg",
-            volume: 30
-        },
-        position: "left wall",
-        coast: {
-            currency: "BYN",
-            volume: 450
-        }
-    }, {
-        material: "tree",
-        color: "light brown",
-        size: "small",
-        weight: {
-            scale: "kg",
-            volume: 10
-        },
-        position: "left wall",
-        coast: {
-            currency: "BYN",
-            volume: 200
-        }
-    }, {
-        material: "fabric",
-        color: "wight",
-        size: "large",
-        weight: {
-            scale: "kg",
-            volume: 35
-        },
-        position: "left wall",
-        coast: {
-            currency: "BYN",
-            volume: 1650
-        }
-    }
-];
+// let myRoom = [
+//     {
+//         material: "fabric",
+//         color: "gray",
+//         size: "lage",
+//         weight:
+//             {
+//                 scale: "kg",
+//                 volume: 50
+//             },
+//         position: "right wall",
+//         coast:
+//             {
+//                 currency: "BYN",
+//                 volume: 1500
+//             }
+//     }, {
+//         material: "tree",
+//         color: "yellow",
+//         size: "medium",
+//         weight:
+//             {
+//                 scale: "kg",
+//                 volume: 10
+//             },
+//         position: "window",
+//         coast:
+//             {
+//                 currency: "BYN",
+//                 volume: 250
+//             },
+//     }, {
+//         material: "skin",
+//         color: "brown",
+//         size: "semi-medium",
+//         weight: {
+//             scale: "kg",
+//             volume: 15
+//         },
+//         position: "center",
+//         coast:
+//             {
+//                 currency: "BYN",
+//                 volume: 850
+//             }
+//     }, {
+//         material: "metal",
+//         color: "black",
+//         size: "small",
+//         weight: {
+//             scale: "kg",
+//             volume: 0.3
+//         },
+//         position: "on the table",
+//         coast: {
+//             currency: "BYN",
+//             volume: 25
+//         }
+//     }, {
+//         material: "skin",
+//         color: "purple",
+//         size: "semi medium",
+//         weight: {
+//             scale: "kg",
+//             volume: 5
+//         },
+//         position: "left angle",
+//         coast: {
+//             currency: "BYN",
+//             volume: 380
+//         }
+//     }, {
+//         material: "tree",
+//         color: "light brown",
+//         size: "medium",
+//         weight: {
+//             scale: "kg",
+//             volume: 30
+//         },
+//         position: "left wall",
+//         coast: {
+//             currency: "BYN",
+//             volume: 450
+//         }
+//     }, {
+//         material: "tree",
+//         color: "light brown",
+//         size: "small",
+//         weight: {
+//             scale: "kg",
+//             volume: 10
+//         },
+//         position: "left wall",
+//         coast: {
+//             currency: "BYN",
+//             volume: 200
+//         }
+//     }, {
+//         material: "fabric",
+//         color: "wight",
+//         size: "large",
+//         weight: {
+//             scale: "kg",
+//             volume: 35
+//         },
+//         position: "left wall",
+//         coast: {
+//             currency: "BYN",
+//             volume: 1650
+//         }
+//     }
+// ];
+//
+// myRoom[7].coast.volume = 10
+// console.log(myRoom[7].coast.volume);
+// console.log(myRoom);
 
-myRoom[7].coast.volume = 10
-console.log(myRoom[7].coast.volume);
-console.log(myRoom);
+// let shopKorona = [
+//     {
+//         nameDepartment: "Pet Products",
+//         numberRacks: 3,
+//         numberRegisters: null,
+//         goods: {
+//             good1: "Pet Food",
+//             good2: "Pet toy",
+//             good3: "Other",
+//             good4: null,
+//             good5: null
+//         }
+//     },
+//     {
+//         nameDepartment: " Household goods",
+//         numberRacks: 4,
+//         numberRegisters: null,
+//         goods: {
+//             good1: "Kitchen goods",
+//             good2: "Bathroom goods",
+//             good3: "Other",
+//             good4: null,
+//             good5: null
+//         }
+//     },
+//     {
+//         nameDepartment: "Grocery ",
+//         numberRacks: 2,
+//         numberRegisters: null,
+//         goods: {
+//             good1: "Buns",
+//             good2: "Bread",
+//             good3: "Sweets",
+//             good4: null,
+//             good5: null
+//         }
+//     },
+//     {
+//         nameDepartment: "Ready food",
+//         numberRacks: 3,
+//         numberRegisters: null,
+//         goods: {
+//             good1: "Garnish",
+//             good2: "Meat",
+//             good3: "Fish",
+//             good4: null,
+//             good5: null
+//         }
+//     },
+//     {
+//         nameDepartment: "Dairy products",
+//         numberRacks: 4,
+//         numberRegisters: 2,
+//         goods: {
+//             good1: "Milk",
+//             good2: "Cheese",
+//             good3: "Other",
+//             good4: null,
+//             good5: null
+//         }
+//     },
+//     {
+//         nameDepartment: "Alcohol",
+//         numberRacks: 8,
+//         numberRegisters: null,
+//         goods: {
+//             good1: "Beer",
+//             good2: "Wines",
+//             good3: "Whiskey",
+//             good4: "Champagne",
+//             good5: "Brandy"
+//         }
+//     },
+//     {
+//         nameDepartment: "Freezing",
+//         numberRacks: 6,
+//         numberRegisters: null,
+//         goods: {
+//             good1: "Meat",
+//             good2: "Fish",
+//             good3: "Dumplings",
+//             good4: "Shrimps",
+//             good5: "Vegetables"
+//         }
+//     },
+//     {
+//         nameDepartment: "Cash registers",
+//         numberRacks: 14,
+//         numberRegisters: 14,
+//         goods: {
+//             good1: "Discouted products",
+//             good2: null,
+//             good3: null,
+//             good4: null,
+//             good5: null
+//         }
+//     },
+//
+//
+// ];
 
+
+console.log(shopKorona[7].numberRegisters);
