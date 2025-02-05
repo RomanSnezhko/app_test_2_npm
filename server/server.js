979bfb2f66fca38c7d9459e32ca94aea1dcb89b2
RomanSnezhko <roman.snezhko@bk.ru>
2025-02-05 19:11:10 +0300

    add server 1.1

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
-
-
-
 const http = require('http');
+
 let requestsCount = 0
 
 const server = http.createServer((reqest, response) => {
+    requestsCount++;
+    switch (reqest.url) {
+        case '/students':
+            response.write('Students');
+            break;
+        case '/':
+        case '/courses':
+            response.write('Front+Back');
+            break;
+        default:
+            response.write('404: Not Found');
+    }
+    response.write('requests: ' + requestsCount);
 
-    requestsCount++
-
-    response.write('Hi: ' + requestsCount)
     response.end()
 })
 server.listen(3003)
