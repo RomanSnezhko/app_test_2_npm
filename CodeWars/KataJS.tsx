4407941121b6e052f021f56258921a029366f265
RomanSnezhko <roman.snezhko@bk.ru>
2025-02-17 16:54:53 +0300

    my_JS

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -460,7 +460,11 @@ Description:
 
 Solution:
 */
-
+        function warnTheSheep(queue) {
+            const index=queue.indexOf('wolf')
+            return index===queue.length-1?"Pls go away and stop eating my sheep"
+                :`Oi! Sheep number ${queue.length-1-index}! You are about to be eaten by a wolf!`
+        }
 /*
 6 kyu
 
