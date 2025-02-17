0d5adde03ecd18f04f5cfffddf7be75ab7795102
RomanSnezhko <roman.snezhko@bk.ru>
2025-02-17 17:01:41 +0300

    my_JS

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -542,7 +542,12 @@ Description:
 
 Solution:
 */
-
+        function closest(arr) {
+            return arr.sort((a, b) => Math.abs(a) - Math.abs(b)).includes(arr[0] * -1) &&
+            arr[0] !== 0
+                ? null
+                : arr[0];
+        }
 /*
 6 kyu
 
