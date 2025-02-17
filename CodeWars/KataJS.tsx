c04ecb16f63e5aa028e9ab1d1b3c30fc4402b0a0
RomanSnezhko <roman.snezhko@bk.ru>
2025-02-17 16:54:09 +0300

    my_JS

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -435,7 +435,10 @@ Description:
 
 Solution:
 */
-
+        function datingRange(age){
+            return age>=14?`${Math.floor(age/2+7)}-${Math.floor((age-7)*2)}`:
+                `${Math.floor(age-(age*0.1))}-${Math.floor(age+(age*0.1))}`
+        }
 /*
 6 kyu
 
