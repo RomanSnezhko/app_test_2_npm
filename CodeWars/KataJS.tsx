8566050bdf0ffb0a96e692b788a404cf746fe92a
RomanSnezhko <roman.snezhko@bk.ru>
2025-02-18 16:27:29 +0300

    my_JS

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -524,15 +524,7 @@ Description:
 
 Solution:
 */
-        const prevMultOfThree = n => {
-            let str = n.toString().split('').reverse('').join('')
-            for(let i=0;i<str.length;i++){
-                if(str.slice(i).split('').reverse('').join('')*1%3===0){
-                    return str.slice(i).split('').reverse('').join('')*1
-                }
-            }
-            return null
-        }
+
 /*
 6 kyu
 
