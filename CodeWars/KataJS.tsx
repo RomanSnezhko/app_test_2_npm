011e016bc2a6c19f556429ab834ea3eb36cb9842
RomanSnezhko <roman.snezhko@bk.ru>
2025-02-18 16:27:40 +0300

    my_JS

--- CodeWars/KataJS.tsx
+++ CodeWars/KataJS.tsx
@@ -505,16 +505,7 @@ Description:
 
 Solution:
 */
-        class Ship {
-            constructor(draft, crew) {
-                this.draft = draft;
-                this.crew = crew;
-            }
 
-            isWorthIt() {
-                return this.draft - 1.5 * this.crew > 20;
-            }
-        }
 /*
 6 kyu
 
